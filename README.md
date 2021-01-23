2df2a7d3fd53ad9e8a28dc55d16eee9f6cd4ce46
Jordan Zucker <jordan.zucker@gmail.com>
2021-01-23 02:21:34 -0800

    This is much cleaner, still a little funky, can't wait to test

--- README.md
+++ README.md
@@ -22,3 +22,7 @@ python3 http_cli.py all-off
 ```
 http://10.0.1.104:5000/api/v1/activities/all-off?kitchen=0&dining_room=0
 ```
+
+### Raspberry Pi References
+
+For how to handle buttons, click [here](https://gpiozero.readthedocs.io/en/stable/recipes.html#button)
--- src/pi_client.py
+++ src/pi_client.py
@@ -1,5 +1,6 @@
 import logging
 from gpiozero import Button
+from signal import pause
 from .activities import Activities, ActivityName
 
 
@@ -7,9 +8,6 @@ log = logging.getLogger(__name__)
 
 
 class PiClient(object):
-    def __init__(self, rufus_client):
-        self.rufus_client = rufus_client
-
     BUTTONS = {
         ActivityName.ALL_OFF: Button(Activities.get_activity_pin(ActivityName.ALL_OFF), bounce_time=0.5),
         ActivityName.APPLE_TV: Button(Activities.get_activity_pin(ActivityName.APPLE_TV), bounce_time=0.5),
@@ -17,27 +15,17 @@ class PiClient(object):
         ActivityName.BEDTIME: Button(Activities.get_activity_pin(ActivityName.BEDTIME), bounce_time=0.5),
     }
 
+    def __init__(self, rufus_client):
+        self.rufus_client = rufus_client
+        for activity_name, button in self.BUTTONS.items():
+            def shim_func():
+                self.rufus_client.request_activity(activity_name)
+            button.when_pressed = shim_func()
+
     @classmethod
     def get_button(cls, activity):
         return cls.BUTTONS[activity]
 
     def run(self):
-        while True:
-            if self.get_button(ActivityName.ALL_OFF).is_pressed:
-                print("all off pressed")
-                response = self.rufus_client.request_activity(ActivityName.ALL_OFF)
-                print(f'response => {response}')
-            elif self.get_button(ActivityName.APPLE_TV).is_pressed:
-                print("apple tv is pressed")
-                response = self.rufus_client.request_activity(ActivityName.APPLE_TV)
-                print(f'response => {response}')
-            elif self.get_button(ActivityName.VINYL).is_pressed:
-                print("vinyl is pressed")
-                response = self.rufus_client.request_activity(ActivityName.VINYL)
-                print(f'response => {response}')
-            elif self.get_button(ActivityName.BEDTIME).is_pressed:
-                print("bedtime is pressed")
-                response = self.rufus_client.request_activity(ActivityName.BEDTIME)
-                print(f'response => {response}')
-            else:
-                print("No button is pressed")
+        pause()
+

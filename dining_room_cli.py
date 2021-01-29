92cb7f34fab11bd9a2435d2d59f8405df309e204
Jordan Zucker <jordan.zucker@gmail.com>
2021-01-28 20:50:14 -0800

    Simplify and reduce code repetition

--- dining_room_cli.py
+++ dining_room_cli.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 
 import logging
-from src.rufus_client import RufusClient
-from src.pi_client import PiClient
-from src.arg_parser import create_argparser
-from src.utils import get_log_level
+from src.remote_process import process
 from src.configs.dining_room import DiningRoomConfig
 
 
@@ -12,15 +9,8 @@ log = logging.getLogger(__name__)
 
 
 def main():
-    arg_parser = create_argparser()
-    args = arg_parser.parse_args()
-    log_level = get_log_level(args.debug)
-    logging.basicConfig(level=log_level)
-    log.info(f'Set log_level to {log_level}')
-    rufus_client = RufusClient(ip=args.ip)
-    dining_room_config = DiningRoomConfig()
-    pi_client = PiClient(rufus_client, dining_room_config, debug=args.debug)
-    pi_client.run()
+    log.info('Starting dining room ...')
+    process(DiningRoomConfig)
 
 if __name__ == '__main__':
     main()
---
+++ src/remote_process.py
@@ -0,0 +1,21 @@
+import logging
+from .rufus_client import RufusClient
+from .pi_client import PiClient
+from .arg_parser import create_argparser
+from .utils import get_log_level
+
+
+log = logging.getLogger(__name__)
+
+
+def process(config_class):
+    arg_parser = create_argparser()
+    args = arg_parser.parse_args()
+    log_level = get_log_level(args.debug)
+    logging.basicConfig(level=log_level)
+    log.info(f'Set log_level to {log_level}')
+    rufus_client = RufusClient(ip=args.ip)
+    living_room_config = config_class()
+    pi_client = PiClient(rufus_client, living_room_config, debug=args.debug)
+    # run as well
+    pi_client.run()

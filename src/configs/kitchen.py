b35205da93b347531048f105ac19f5fcaa6068fd
Jordan Zucker <jordan.zucker@gmail.com>
2021-02-01 14:52:01 -0800

    Set up remote configs for real for other rooms

--- src/configs/kitchen.py
+++ src/configs/kitchen.py
@@ -15,6 +15,30 @@ class KitchenConfig(ControllerConfig):
         ])
 
     @property
+    def has_volume_rotary_encoder(self):
+        return True
+
+    @property
+    def volume_rotary_encoder_pins(self):
+        return list([
+            Constants.ROTARY_RED_WIRE_PIN, Constants.ROTARY_YELLOW_WIRE_PIN, Constants.ROTARY_BLUE_WIRE_PIN
+        ])
+
+    @property
+    def has_volume_domain_switch(self):
+        return True
+
+    @property
+    def volume_domain_switch_pins(self):
+        return list([
+            Constants.GREEN_VOLUME_DOMAIN_WIRE_PIN
+        ])
+
+    @property
+    def local_volume_activity_name(self):
+        return ActivityName.KITCHEN_VOLUME_ADJUSTMENT
+
+    @property
     def activities(self):
         return {
             ActivityName.ALL_OFF: Constants.BLUE_WIRE_PIN,
